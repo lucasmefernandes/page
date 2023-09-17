807d10b7b4da855abd29ff53388d879a433e06b4
Lucas Messias <100249992+lucasmefernandes@users.noreply.github.com>
2023-09-17 15:07:11 +0100

    Alterações: Logica

---
+++ js-slide/dados.js
@@ -0,0 +1,24 @@
+const dadosSliders = [
+  {
+    imagens: [
+      "assets/imgs/imgs-slides/1.png",
+      "assets/imgs/imgs-slides/2.png",
+      "assets/imgs/imgs-slides/3.png",
+      "assets/imgs/imgs-slides/4.png",
+      "assets/imgs/imgs-slides/5.png"
+    ],
+    nome: "Slider 1"
+  },
+  {
+    imagens: [
+      "assets/imgs/imgs-slides/rolete1.png",
+      "assets/imgs/imgs-slides/rolete2.png",
+      "assets/imgs/imgs-slides/rolete3.png",
+      "assets/imgs/imgs-slides/rolete4.png",
+      "assets/imgs/imgs-slides/rolete5.png"
+    ],
+    nome: "Slider 2"
+  }
+];
+
+export default dadosSliders;
--- js-slide/slide.js
+++ js-slide/slide.js
@@ -1,41 +1,89 @@
+import dadosSliders from "./dados.js";
+
+const dadosObtidos = dadosSliders
+
+
+const contPrincipal = document.querySelector(".slider-main");
+
+
+dadosObtidos.forEach((dadosSlider, index) => {
+
+  const slider = document.createElement("div");
+  slider.classList.add("slider");
+
+ 
+  const slides = document.createElement("div");
+  slides.classList.add("slides");
+
+  dadosSlider.imagens.forEach((imagem, i) => {
+
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.name = `radio-btn-${i}`;
+    radio.classList.add("radio-btn");
+    radio.id = `radio${i + 1}-${i}`;
+
+    const rotulo = document.createElement("label");
+    rotulo.htmlFor = `radio${i + 1}-${i}`;
+    rotulo.classList.add("manual-btn");
+
+
+    const img = document.createElement("img");
+    img.src = imagem;
+    img.alt = `img${i + 1}`;
+
+
+    const slide = document.createElement("div");
+    slide.classList.add("slide");
+    slide.appendChild(img);
+
+    slides.appendChild(radio);
+    slides.appendChild(rotulo);
+    slides.appendChild(slide);
+  });
+
+
+  slider.appendChild(slides);
+
+  const carga = document.createElement("div");
+  carga.classList.add("load");
+
+
+  slider.appendChild(carga);
+  contPrincipal.appendChild(slider);
+});
+
 let count = 1;
 let interval;
 
 let slider = document.querySelector(".slider");
+let loadBar = document.querySelectorAll(".load");
+
+let loadBars = Array.from(loadBar);
+
+loadBars.forEach(load => load.style.display = "none"); 
+
 slider.addEventListener("mouseover", () => {
+  loadBars.forEach(load => load.style.display = "block");
   interval = setInterval(function () {
     nextImage();
+    loadBars.forEach(load => {
+      load.style.animation = "none";
+      void load.offsetWidth;
+      load.style.animation = "loading 1.6s linear infinite";
+    });
   }, 1600);
-
-  function nextImage() {
-    count++;
-    if (count > 5) {
-      count = 1;
-    }
-    document.getElementById("radio" + count).checked = true;
-  }
 });
 
 slider.addEventListener("mouseout", () => {
+  loadBars.forEach(load => load.style.display = "none");
   clearInterval(interval);
 });
 
-// script-second-slide
-let slider2 = document.querySelector(".slider2");
-slider2.addEventListener("mouseover", () => {
-  interval = setInterval(function () {
-    nextImage();
-  }, 1600);
-
-  function nextImage() {
-    count++;
-    if (count > 5) {
-      count = 1;
-    }
-    document.getElementById("radio2-" + count).checked = true;
+function nextImage() {
+  count++;
+  if (count > 5) {
+    count = 1;
   }
-});
-
-slider2.addEventListener("mouseout", () => {
-  clearInterval(interval);
-});
+  document.getElementById("radio" + count).checked = true;
+}
